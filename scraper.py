829144dcc9320c12c69f68509a312081b5a4527e
Nelson Ogueh <nelsonovovwe@gmail.com>
2022-01-08 23:33:32 +0100

    Feat: Scrape multiple groups in a single session

--- scraper.py
+++ scraper.py
@@ -39,7 +39,7 @@ if not client.is_user_authorized():
     client.send_code_request(phone)
     os.system('clear')
     banner()
-    client.sign_in(phone, input(gr+'[+] Enter the code: '+re))
+    client.sign_in(phone, input(gr+'[+] Enter login code sent to you now: '+re))
  
 os.system('clear')
 banner()
@@ -64,39 +64,62 @@ for chat in chats:
     except:
         continue
  
-print(gr+'[+] Choose a group to scrape members :'+re)
+print(gr+'[+] Choose a group to scrape members: '+re)
 i=0
 for g in groups:
     print(gr+'['+cy+str(i)+gr+']'+cy+' - '+ g.title)
     i+=1
- 
-print('')
-g_index = input(gr+"[+] Enter a Number : "+re)
-target_group=groups[int(g_index)]
- 
-print(gr+'[+] Fetching Members...')
-time.sleep(1)
-all_participants = []
-all_participants = client.get_participants(target_group, aggressive=True)
- 
-print(gr+'[+] Saving In file...')
-time.sleep(1)
-with open("members.csv","w",encoding='UTF-8') as f:
-    writer = csv.writer(f,delimiter=",",lineterminator="\n")
-    writer.writerow(['username','user id', 'access hash','name','group', 'group id'])
-    for user in all_participants:
-        if user.username:
-            username= user.username
-        else:
-            username= ""
-        if user.first_name:
-            first_name= user.first_name
-        else:
-            first_name= ""
-        if user.last_name:
-            last_name= user.last_name
-        else:
-            last_name= ""
-        name= (first_name + ' ' + last_name).strip()
-        writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id])      
-print(gr+'[+] Members scraped successfully.')
+
+
+def fetchAndSaveGroupContacts():
+    print('')
+    g_index = input(gr+"[+] Enter a number corresponding to a group listed above or type EXIT to exit the process: "+cy)
+    if  g_index.lower() == "EXIT".lower():
+        exit()
+    elif (int(g_index) > len(groups)):
+        print("The number supplied cannot be greater than the greatest number in the list above. Please try again.")
+        print('')
+        print('')
+        fetchAndSaveGroupContacts()
+    elif g_index.isdigit():
+        target_group=groups[int(g_index)]
+    else:
+        print("Sorry! You've supplied an invalid input. Please try again.")
+        print('')
+        print('')
+        fetchAndSaveGroupContacts()
+      
+
+
+    print("TARGET GROUP: "+target_group.title)
+    
+    print(gr+'[+] Fetching Members...')
+    time.sleep(1)
+    all_participants = []
+    all_participants = client.get_participants(target_group, aggressive=True)
+    
+    print(gr+'[+] Saving In file...')
+    time.sleep(1)
+    with open(target_group.title.replace("/", "_")+".csv","w", encoding='UTF-8') as f:
+        writer = csv.writer(f,delimiter=",",lineterminator="\n")
+        writer.writerow(['username','user id', 'access hash','name','group', 'group id'])
+        for user in all_participants:
+            if user.username:
+                username= user.username
+            else:
+                username= ""
+            if user.first_name:
+                first_name= user.first_name
+            else:
+                first_name= ""
+            if user.last_name:
+                last_name= user.last_name
+            else:
+                last_name= ""
+            name= (first_name + ' ' + last_name).strip()
+            writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id])      
+    print(gr+'[+] Members successfully scraped and saved as '+gr+target_group.title+".csv")
+    fetchAndSaveGroupContacts()
+
+fetchAndSaveGroupContacts()
+
